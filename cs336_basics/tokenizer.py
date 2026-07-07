7124e40ed42ba4177a67d010fed9e79766795a4e
Uxvan <lih337831@gmail.com>
2026-07-07 23:03:39 +0800

    Enhance tokenizer encoding method
    Refactor encoding logic to use byte-to-ID mapping and improve merge handling.

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -5,6 +5,8 @@ class Tokenizer:
         self.vocab=vocab
         self.merges=merges
         self.special_tokens=special_tokens
+        self.byte_to_id={v:k for k,v in self.vocab.items()}
+        self.merges_to_id=[(self.byte_to_id[x],self.byte_to_id[y]) for x,y in merges]
 
     @classmethod
     def from_files(cls, vocab_filepath:str, merges_filepath, special_tokens=None):
@@ -16,29 +18,32 @@ class Tokenizer:
         return cls(vocab,merges,special_tokens)
 
     def encode(self,text:str)->list[int]:
-        b_txt=text.encode()
-        int_txt=list(b_txt)
-        int_pairs=list(zip(int_txt[:-1],int_txt[1:]))
 
-        #找应该被合并的项
-        i=0
-        while i<len(int_pairs):
-            p=int_pairs[i]
-            byte_p=(self.vocab[p[0]],self.vocab[p[1]])
-            merged_bytes=bytes(p)
-            if byte_p in self.merges:
-                num=next(k for k,v in self.vocab.items() if v==merged_bytes)
-                int_txt[i]=num
-                del int_txt[i+1]
-                if i>0:
-                    int_pairs[i-1][1]=num
-                if i<len(int_pairs)-1:
-                    int_pairs[i+1][0]=num
-                del int_pairs[i]
-            else:
-                i+=1
-        return int_txt
-    
+        byte_txt=[bytes([x]) for x in list(text.encode())]
+        id_txt=[self.byte_to_id[byte_txt[i:i+1]] for i in range(byte_txt)]
+        id_pairs=list(zip(id_txt[:-1],id_txt[1:]))
+
+        #按merges优先级找应该被合并的项
+        j=0
+        while j<len(self.merges):
+            goal_id_pair=self.merges_to_id[j]
+            goal_merged_byte=self.vocab[goal_id_pair[0]]+self.vocab[goal_id_pair[1]]
+            new_id=self.byte_to_id[goal_merged_byte]
+            i=0
+            while i<len(id_pairs):
+                if id_pairs[i]==goal_id_pair:
+                    id_txt[i]=new_id
+                    if i>0:
+                        id_pairs[i-1]=(id_txt[i-1],new_id)
+                    if i<len(id_pairs)-1:
+                        id_pairs[i+1]=(new_id,id_txt[i+2])
+                    del id_pairs[i]
+                else:
+                    i+=1
+            j+=1
+        return id_txt
+        
+        
     def encode_iterable(self,iterable):
        for text in iterable:
            yield from self.encode(text)
@@ -48,7 +53,7 @@ class Tokenizer:
         for i in ids:
             bytes.append(self.vocab[i])
         result=b''.join(bytes)
-        text=result.decode('utf-8')
+        text=result.decode('utf-8',errors='ignore')
         return text
     
 
