fbaa9f06e0a1d1f36f9b55585f76286aa2cc1dc2
Uxvan <lih337831@gmail.com>
2026-07-07 15:15:00 +0800

    Refactor Tokenizer methods for clarity and efficiency

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -1,26 +1,30 @@
 import pickle
+
 class Tokenizer:
     def __init__(self,vocab,merges,special_tokens=None):
         self.vocab=vocab
         self.merges=merges
         self.special_tokens=special_tokens
+
+    @classmethod
     def from_files(cls, vocab_filepath:str, merges_filepath, special_tokens=None):
         with open(vocab_filepath,'rb') as f:
             vocab=pickle.load(f)
         with open(merges_filepath,'rb') as f:
             merges=pickle.load(f)
         special_tokens=special_tokens
+        return cls(vocab,merges,special_tokens)
 
     def encode(self,text:str)->list[int]:
         b_txt=text.encode()
         int_txt=list(b_txt)
-        int_pairs=zip(int_txt[:-1],int_txt[1:])
+        int_pairs=list(zip(int_txt[:-1],int_txt[1:]))
 
         #找应该被合并的项
         i=0
-        while i<len(list(int_pairs)):
+        while i<len(int_pairs):
             p=int_pairs[i]
-            byte_p=(self.vocab(p[0]),self.vocab(p[1]))
+            byte_p=(self.vocab[p[0]],self.vocab[p[1]])
             merged_bytes=bytes(p)
             if byte_p in self.merges:
                 num=next(k for k,v in self.vocab.items() if v==merged_bytes)
@@ -40,10 +44,11 @@ class Tokenizer:
            yield from self.encode(text)
 
     def decode(self, ids:list[int]) -> str:
-        bytes=''
+        bytes=[]
         for i in ids:
-            bytes+=self.vocab[i]
-        text=bytes.decode('utf-8')
+            bytes.append(self.vocab[i])
+        result=b''.join(bytes)
+        text=result.decode('utf-8')
         return text
     
 
