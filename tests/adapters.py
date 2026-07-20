30d93dc1daec24c5d2dd873470803b81fe8b2598
Uxvan <lih337831@gmail.com>
2026-07-20 14:50:03 +0800

    Update adapters.py

--- tests/adapters.py
+++ tests/adapters.py
@@ -388,7 +388,12 @@ def run_rmsnorm(
         Float[Tensor,"... d_model"]: Tensor of with the same shape as `in_features` with the output of running
         RMSNorm of the `in_features`.
     """
-    raise NotImplementedError
+    #raise NotImplementedError
+    from cs336_basics.transformer_lm import RMSNorm
+    norm=RMSNorm(d_model,eps)
+    with torch.no_grad():
+        norm.g.copy_(weights)
+    return norm(in_features)
 
 
 def run_silu(in_features: Float[Tensor, " ..."]) -> Float[Tensor, " ..."]:
