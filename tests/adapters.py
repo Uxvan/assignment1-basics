572fe46bb7b4f47ae4325fc580fae2f5ae0082c1
Uxvan <lih337831@gmail.com>
2026-07-21 20:56:34 +0800

    Update adapters.py

--- tests/adapters.py
+++ tests/adapters.py
@@ -159,7 +159,7 @@ def run_multihead_self_attention(
         implementation with the given QKV projection weights and input features.
     """
     #raise NotImplementedError
-    from cs336_baiscs.transformer_lm import MultiHeadselfAttention
+    from cs336_basics.transformer_lm import MultiHeadselfAttention
     mha=MultiHeadselfAttention(d_model, num_heads)
     with torch.no_grad():
         mha.W_Q.weight.copy_(q_proj_weight)
@@ -207,7 +207,7 @@ def run_multihead_self_attention_with_rope(
         implementation with the given QKV projection weights and input features.
     """
     #raise NotImplementedError
-    from cs336_baiscs.transformer_lm import MultiHeadselfAttention
+    from cs336_basics.transformer_lm import MultiHeadselfAttention
     mha=MultiHeadselfAttention(d_model, num_heads)
     with torch.no_grad():
         mha.W_Q.weight.copy_(q_proj_weight)
