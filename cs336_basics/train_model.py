d6ea60715a79e0d4359e196580590a0ca53b0625
Uxvan <lih337831@gmail.com>
2026-07-22 23:14:20 +0800

    Update train_model.py

--- cs336_basics/train_model.py
+++ cs336_basics/train_model.py
@@ -4,7 +4,7 @@ def cross_entropy(logits:torch.tensor, targets:torch.tensor):
     logits: 模型输出的原始分数, (batch_size, vocab_size)
     targets: i.e. 序列位置i对应的正确答案的token_id, (batch_size,)
     """
-    logits=logits-torch.max(logits,dim=-1,keepdim=True).values() # 保证数值稳定，防止exp过大
+    logits=logits-torch.max(logits,dim=-1,keepdim=True).values # 保证数值稳定，防止exp过大
     exp_logits=torch.exp(logits)
     targets_lgts=exp_logits[torch.arange(logits.shape[0]), targets] # (batch_size,)
 
