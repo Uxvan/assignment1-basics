b5ad237ddd5c53ae97a09e8f528d5a24d7286e95
Uxvan <lih337831@gmail.com>
2026-07-23 23:39:23 +0800

    Update adapters.py

--- tests/adapters.py
+++ tests/adapters.py
@@ -607,7 +607,9 @@ def run_save_checkpoint(
             we've completed.
         out (str | os.PathLike | BinaryIO | IO[bytes]): Path or file-like object to serialize the model, optimizer, and iteration to.
     """
-    raise NotImplementedError
+    #raise NotImplementedError
+    from cs336_basics.train_model import save_checkpoint
+    save_checkpoint(model,optimizer,iteration,out)
 
 
 def run_load_checkpoint(
@@ -628,7 +630,9 @@ def run_load_checkpoint(
     Returns:
         int: the previously-serialized number of iterations.
     """
-    raise NotImplementedError
+    #raise NotImplementedError
+    from cs336_basics.train_model import load_checkpoint
+    return load_checkpoint(src,model,optimizer)
 
 
 def get_tokenizer(
