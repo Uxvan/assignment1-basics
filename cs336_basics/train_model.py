0631348e90feea195c7840fcb6f879c0bc8d47d4
Uxvan <lih337831@gmail.com>
2026-07-23 14:23:23 +0800

    Update train_model.py

--- cs336_basics/train_model.py
+++ cs336_basics/train_model.py
@@ -13,3 +13,39 @@ def cross_entropy(logits:torch.tensor, targets:torch.tensor):
     loss=sum.log() - targets_lgts    # (batch_size,),不直接log是为了防止某个logit对应值过小，导致下溢出现-inf
     loss=loss.mean(dim=0)         
     return loss
+
+
+class AdamW(torch.optim.Optimizer):
+    def __init__(self, params, alpha, beta_1, beta_2, gamma, eps):
+        defaults=dict(alpha=alpha, beta_1=beta_1, beta_2=beta_2, eps=eps, gamma=gamma)
+        super().__init__(params,defaults)
+    
+    @torch.no_grad()
+    def step(self, closure=None):
+        loss=None
+        if closure is not None:
+            with torch.enable_grad():
+                loss=closure()
+
+        for group in self.param_groups:
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+                
+                grad=p.grad.data
+                state=self.state[p]
+
+                if len(state)==0:
+                    state["steps"]=0
+                    state['m']=torch.zeros_like(p)
+                    state['v']=torch.zeros_like(p)
+
+                state["steps"]+=1
+                t=state["steps"]
+                group['alpha']=group['alpha'] * (1-group['beta_2']**t).sqrt() / (1-group['beta_1']**t)
+                p -= group['alpha']*group['gamma']*p
+                state['m']=state['m']*group['beta_1']+(1-group['beta_1'])*grad
+                state['v']=state['v']*group['beta_2']+(1-group['beta_2'])*grad*grad
+                p -= group['alpha']*state['m']/((state['v']).sqrt()+group['eps'])
+
+        return loss
